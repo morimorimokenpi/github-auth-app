a2a48697d21318a8d7e008bfee968f35c5e40a55
Yasunori Kobayashi <52128219+morimorimokenpi@users.noreply.github.com>
2021-06-09 00:05:57 +0900

    :sparkles: moromorojissou

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,59 @@
 import React from 'react';
+import { ApolloProvider } from 'react-apollo';
+import client from './client';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
 
-const App = () => {
+const User = gql`
+  query UserInfo($login: String!, $parNum: Int) {
+    user(login: $login) {
+      name
+      avatarUrl
+      pullRequests(first: $parNum) {
+        nodes {
+          id
+          title
+          permalink
+        }
+      }
+    }
+  }
+`;
+
+interface pullRequestData {
+  id: string;
+  title: string;
+  permalink: string;
+}
+
+const App: React.FC = () => {
   return (
-    <div>
-      <p>Hello GraphQl</p>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <p>Hello GraphQL</p>
+        <Query query={User} variables={{ login: 'morimorimokenpi', parNum: 30 }}>
+          {(result: any) => {
+            const { loading, error, data} = result;
+
+            if (loading) return <p>Loading...</p>
+            if (error) return <p>Error {error.message}</p>
+
+            return (
+              <div>
+                <p>{data.user.name}</p>
+                <img src={data.user.avatarUrl} />
+                {data.user.pullRequests.nodes.map((c: pullRequestData) => (
+                  <div key={c.id}>
+                    <p>{c.title}</p>
+                    <a href={c.permalink}>Github</a>
+                  </div>
+                ))}
+              </div>
+            )
+          }}
+        </Query>
+      </div>
+    </ApolloProvider>
   );
 }
 
